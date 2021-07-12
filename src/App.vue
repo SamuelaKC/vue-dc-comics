c4aaaf83b8f537db6acc3d56e9f5da18d06930a5
SamuelaKC <carnelli.samuela@gmail.com>
2021-07-12 15:06:57 +0200

    inizio

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
+/* Esercizio di oggi: Vue DC Comics
+nome repo: vue-dc-comics
+Descrizione:
+Create un nuovo progetto utilizzando Vue CLI e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando i data del componente. 
+Per oggi diamo priorità alla struttura: quando è tutto bello solido, passiamo al Sass!
+Bonus:
+Creare un componente aggiuntivo per gestire la fascia azzurra con le icone.
+Da ricordare:
+creare il nuovo progetto, modalità manuale, installando sass (guardate la parte finale delle slide di venerdì)
+quando sviluppiamo lanciamo e teniamo aperto il comando npm run serve
+prima del push, CTRL + C per chiudere il processo di npm run serve, e poi
+npm run build
+La prima volta sarà necessario eliminare dist/ dal .gitignore.
+Qui la repo di oggi: https://github.com/ott-fogliata/vue-molisana
+In allegato tutto il materiale per l’esercizio. */
+
+
+
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Main />
+    <MainSectionBlue />
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'; 
+import Main from './components/Main.vue';
+import MainSectionBlue from './components/MainSectionBlue.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header, 
+    Main, 
+    MainSectionBlue, 
+    Footer
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/MainSectionBlue.vue
@@ -0,0 +1,14 @@
+<template>
+<div>MainSectionBlue</div>
+    
+</template>
+
+
+<script>
+export default {
+    name: 'MainSectionBlue'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
